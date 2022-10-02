71ffbd7ebaec12c9d32f7a1e10fb18414143dc32
breng1 <brendaknguyen@gmail.com>
2022-10-01 19:13:48 -0700

    Add game() pseudocode & implement results display
    The game() function runs five rounds of Rock, Paper, Scissors,
    and displays the final winner. Add logic for displaying
    the final winner of the game. Add pseudocode to indicate where
    helper functions will be used in the game() funciton.

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 // script.js
+
+// Plays five rounds of RPS, keeps track of score, and displays winner
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+
+    // Play five rounds of RPS
+    for (let i = 0; i < 5; i++) {
+        // Prompt user for input
+        // Generate commputer choice
+        // Play single round
+        // Update scores
+        // Display current scores
+    }
+
+    // Display final game results
+    if (playerScore > computerScore) {
+        console.log("Game Over. You won!");
+    }
+    else if (computerScore > playerScore){
+        console.log("Game Over. You lost.");
+    }
+    else {
+        console.log("Game Over. It's a tie!");
+    }
+};
